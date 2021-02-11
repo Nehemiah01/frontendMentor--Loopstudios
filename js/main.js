4edda4233330aa8dbed07a47e5a71916d756c41f
Dapo Ola-Olatunji <oladapo.oon@gmail.com>
2021-02-11 15:22:53 +0100

    no major change

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-console.log("this is my first project on Frontend mentor")
-const navbar = document.querySelector("nav");
-navbar.onclick
+// console.log("this is my first project on Frontend mentor")
+// const navbar = document.querySelector("nav");
+// navbar.onclick
+// let widthWindow = document.window.width
+// console.log(widthWindow)
+console.log(document)
+if (document.body.clientWidth <= 375) {
+    document.querySelector('nav').style.display = 'none'
+} else if (document.body.clientWidth > 375){
+    document.querySelector('nav').style.display = 'inherit'
+}
